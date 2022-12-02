815b5c1a34888d45987f3ed424a3be7741a55e0a
Nikita Babichenko <babichenkona@gmail.com>
2022-12-02 18:52:43 +0300

    init commit

---
+++ resources/js/Components/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Menu = ({header, items, active, setActive}) => {
+    return (
+        <div className={active ? 'menu active' : 'menu'} onClick={() => setActive(false)}>
+            <div className="blur" />
+            <div className="menu__content" onClick={(e) => e.stopPropagation()}>
+                <div className="menu__header">{header}</div>
+                <ul>
+                    {items.map(item =>
+                        <li>
+                            <a href={item.href} className="text-white">{item.value}</a>
+                            <span className="material-icons">{item.icon}</span>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
